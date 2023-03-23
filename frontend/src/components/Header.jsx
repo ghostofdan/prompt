f4c24929e43082e7f166c0f21a47dddfd96f4062
Daniel McKinney <danielmckinney@Daniels-MacBook-Air.local>
2023-03-22 21:12:05 -0400

    more

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div className="bg-gradient-to-r from-[#97D9E1] to=[#D9AFD9]">
+            {/* container */}
+            <div className="container mx-auto max-x-5xl px-2 py-6"></div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
