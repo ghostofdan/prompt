2782e49f2daa6296b33ba768ce4ee6ec2feb77b5
Daniel McKinney <danielmckinney@Daniels-MacBook-Air.local>
2023-03-22 20:59:10 -0400

    test

---
+++ backend/controllers/openaiControllers.js
@@ -0,0 +1,29 @@
+
+
+const { Configuration, OpenAIApi } = require('openai')
+require('dotenv')
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+const generateImage = async (req, res) => {
+    const { prompt } = req.body 
+    console.log(prompt)
+    try {
+        const response = await openai.createImage({
+            prompt: prompt
+        });
+        const image_result = response.data.data
+        res.status(200).json({
+            success: true,
+            data: image_result
+        })
+    } catch(error) {
+        console.log(error)
+    }
+    
+}
+
+module.exports = generateImage;
